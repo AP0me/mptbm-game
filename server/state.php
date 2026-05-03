83a8e186608474f10345ce8fab8371b987750ce9
AP0me <anar.abdullazada@ufaz.az>
2026-05-03 13:46:25 +0400

    ms

--- server/state.php
+++ server/state.php
@@ -91,7 +91,10 @@ function time_passes(int $minutes, array &$state) {
 
     $energy_spent = round(-10 * ($minutes / 60));
     if ($state['sleeping'] ?? false) {
-        $energy_spent = round(-2 * ($minutes / 60));
+        $energy_spent = $energy_spent / 5;
+    }
+    if ($state['shelter'] ?? false) {
+        $energy_spent = $energy_spent / 2;
     }
     add_energy($state, $energy_spent);
 }
@@ -195,12 +198,32 @@ function init_deck(): array {
                 "The player foraged for wood. The lack of visibility made it challenging.";
             }
         ),
+        'shelter' => new Card(
+            'Build a shelter',
+            function(&$state) {
+                return (
+                    in_array(acting_player($state), human_keys()) &&
+                    $state['wood'] > 50
+                );
+            },
+            function(&$state) {
+                $modifier = -((int)(light_level($state) > 0) - 2);
+
+                add_energy($state, -45 * $modifier);
+                time_passes(60 * $modifier, $state);
+
+                $state['shelter'] = true;
+
+                return $modifier === 1 ?
+                "The player built a shelter." :
+                "The player built a shelter. The lack of visibility made it challenging.";
+            }
+        ),
         'fire' => new Card(
             'Make fire with 10 wood',
             function(&$state) {
                 return (
                     in_array(acting_player($state), human_keys()) &&
-                    (pl_dotkey($state, 'energy') > 45 || ($state['fire_minutes'] ?? 0) > 0) &&
                     ($state['wood'] ?? 0) > 0
                 );
             },
